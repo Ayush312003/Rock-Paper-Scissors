ad0b1d337d16cac887dc1139ea4ecdbb59f50918
Ayush Singh <86509024+Ayush312003@users.noreply.github.com>
2022-06-12 21:23:23 +0530

    Update index.php

--- Rock_Papers_Scissors/index.php
+++ Rock_Papers_Scissors/index.php
@@ -6,10 +6,7 @@
 </head>
 <body>
 <div class="container">
-<h1>Welcome to Broken Rock Paper Scissors</h1>
-<p><strong>Note:</strong> This sample code is only
-partially done and serves only as a starting point for the assignment.
-</p>
+<h1>Welcome to Rock Paper Scissors</h1>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
@@ -17,8 +14,6 @@ partially done and serves only as a starting point for the assignment.
 Attempt to go to 
 <a href="game.php">game.php</a> without logging in - it should fail with an error message.
 <p>
-<a href="http://www.wa4e.com/code/rps.zip"
- target="_blank">Source Code for this Application</a>
 </p>
 </div>
 </body>
